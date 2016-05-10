e8917c35687110d7543991f40903f8bf9e6e04d8
Jason Kim <jason@vcnc.co.kr>
2016-05-11 00:22:58 +0900

    change work.c

--- user/work.c
+++ user/work.c
@@ -1,31 +1,28 @@
 #include <core/eos.h>
 
-static eos_tcb_t tcb1;
-static eos_tcb_t tcb2;
-static int8u_t stack1[8096];
-static int8u_t stack2[8096];
-
-/* task1 function - print number 1 to 20 repeatedly */
-static void print_number(void *arg) {
-	int i = 0;
-    while (++i) {
-        printf("%d", i);
-        eos_schedule();         // should stop this task1 then start task2
-        if (i == 20) i = 0;
+    int32u stack1[8096]; // stack for task1
+    int32u stack2[8096]; // stack for task2
+    eos_tcb_t tcb1; // tcb for task1
+    eos_tcb_t tcb2; // tcb for task2
+    /* task1 function - print number 1 to 20 repeatedly */
+    void print_number() {
+        int i = 0;
+        while(++i) {
+            printf("%d", i);
+            _os_schedule(); // 태스크 1 수행 중단, 태스크 2 수행 재개
+            if (i == 20) { i = 0; }
+        }
     }
-}
-
-/* task2 function - print alphabet a to z repeatedly */
-static void print_alphabet(void *arg) {
-	int i = 96;
-    while (++i) {
-        printf("%c", i);
-        eos_schedule();         // should stop this task2 then start task1
-        if (i == 122) i = 96;
+    /* task2 function - print alphabet a to z repeatedly */
+    void print_alphabet() {
+        int i = 96;
+        while(++i) {
+            printf("%c", i);
+            _os_schedule(); // 태스크 2 수행 중단, 태스크 1 수행 재개
+            if (i == 122) { i = 96; }
+        }
     }
-}
-
-void eos_user_main() {
-	eos_create_task(&tcb1, (addr_t)stack1, 8096, &print_number, NULL, 0);
-	eos_create_task(&tcb2, (addr_t)stack2, 8096, &print_alphabet, NULL, 0);
-}
+    void eos_user_main() {
+        eos_create_task(&tcb1, stack1, 8096, print_number, NULL, 0) // 태스크 1 생성
+        eos_create_task(&tcb2, stack2, 8096, print_alphabet, NULL, 0) // 태스크 2 생성
+    }
