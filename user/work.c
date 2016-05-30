94ee69ffaec9168f0bb3cbe55f3aa2dd3a047a96
Jason Kim <jason@vcnc.co.kr>
2016-05-31 02:10:20 +0900

    proj3 fix work.c

--- user/work.c
+++ user/work.c
@@ -5,15 +5,15 @@ eos_tcb_t tcb1; eos_tcb_t tcb2; eos_tcb_t tcb3;
 // tcb for task1 // tcb for task2 // tcb for task3
 
 void task1() {
-    while(1) { PRINT(“A\n”); eos_sleep(0); }
+    while(1) { PRINT("A\n"); eos_sleep(0); }
 }
 
 void task2() {
-    while(1) { PRINT(“B\n”); eos_sleep(0); }
+    while(1) { PRINT("B\n"); eos_sleep(0); }
 }
 
 void task3() {
-    while(1) { PRINT(“C\n”); eos_sleep(0); }
+    while(1) { PRINT("C\n"); eos_sleep(0); }
 }
 void eos_user_main() {
     eos_create_task(&tcb1, stack1, 8096, eos_set_period(&tcb1, 2);
