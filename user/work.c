2f053336af4afa3975c1e705b464564f4754e31f
Jason Kim <jason@vcnc.co.kr>
2016-06-16 01:51:44 +0900

    change work.c

--- user/work.c
+++ user/work.c
@@ -1,43 +1,63 @@
 #include <core/eos.h>
 
-#define WORK_STACK_SIZE 8192
-
-static int8u_t stack1[WORK_STACK_SIZE];    // stack size = WORK_STACK_SIZE bytes
-static int8u_t stack2[WORK_STACK_SIZE];
-static int8u_t stack3[WORK_STACK_SIZE];
+#define WORK_STACK_SIZE 8096
 
 static eos_tcb_t tcb1;
 static eos_tcb_t tcb2;
 static eos_tcb_t tcb3;
 
-static void task1() {
+static int8u_t stack1[WORK_STACK_SIZE];
+static int8u_t stack2[WORK_STACK_SIZE];
+static int8u_t stack3[WORK_STACK_SIZE];
+
+static int8u_t queue1[10];
+static int8u_t queue2[10];
+
+eos_mqueue_t mq1;
+eos_mqueue_t mq2;
+
+static void sender_task(void *arg) {
+    int8u_t *data = "xy";
+
     while (1) {
-        PRINT("A\n");
+        PRINT("send message to mq1\n");
+        eos_send_message(&mq1, data, 0);
+        PRINT("send message to mq2\n");
+        eos_send_message(&mq2, data, 0);
         eos_sleep(0);
     }
 }
 
-static void task2() {
+static void receiver_task1(void *arg) {
+    int8u_t data[2];
+
     while (1) {
-        PRINT("B\n");
+        PRINT("receive message from mq1\n");
+        eos_receive_message(&mq1, data, 0);
+        PRINT("received message: %s\n", data);
         eos_sleep(0);
     }
 }
 
-static void task3() {
+static void receiver_task2(void *arg) {
+    int8u_t data[2];
+
     while (1) {
-        PRINT("C\n");
+        PRINT("receive message from mq2\n");
+        eos_receive_message(&mq2, data, 0);
+        PRINT("received message: %s\n", data);
         eos_sleep(0);
     }
 }
 
 void eos_user_main() {
-    eos_create_task(&tcb1, (addr_t)stack1, WORK_STACK_SIZE, task1, NULL, 1);
+    eos_create_task(&tcb1, (addr_t)stack1, WORK_STACK_SIZE, sender_task, NULL, 50);
+    eos_create_task(&tcb2, (addr_t)stack2, WORK_STACK_SIZE, receiver_task1, NULL, 10);
+    eos_create_task(&tcb3, (addr_t)stack3, WORK_STACK_SIZE, receiver_task2, NULL, 10);
     eos_set_period(&tcb1, 2);
-
-    eos_create_task(&tcb2, (addr_t)stack2, WORK_STACK_SIZE, task2, NULL, 10);
     eos_set_period(&tcb2, 4);
+    eos_set_period(&tcb3, 5);
 
-    eos_create_task(&tcb3, (addr_t)stack3, WORK_STACK_SIZE, task3, NULL, 50);
-    eos_set_period(&tcb3, 8);
+    eos_init_mqueue(&mq1, queue1, 5, 2, FIFO);
+    eos_init_mqueue(&mq2, queue2, 5, 2, FIFO);
 }
